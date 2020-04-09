43f061146ec204145847d43c4c5dae21220300c0
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-04-09 20:35:34 +0900

    HTML＆CSS完了

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     validates :content, presence: true
     mount_uploader :image, ImageUploader
-    belongs_to :user
+    belongs_to :user, optional: true
 end
 
